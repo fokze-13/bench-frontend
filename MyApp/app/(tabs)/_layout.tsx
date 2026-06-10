61caf4522750eca8f1123a69b184867bce59a944
fokze-13 <108113475+fokze-13@users.noreply.github.com>
2026-06-10 19:36:14 +0500

    prettier

--- MyApp/app/(tabs)/_layout.tsx
+++ MyApp/app/(tabs)/_layout.tsx
@@ -1,26 +1,32 @@
-import { Tabs } from 'expo-router';
+import { Tabs } from "expo-router";
 
 export default function TabLayout() {
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: '#ffd33d',
-        headerStyle: {
-          backgroundColor: '#25292e',
-        },
-        headerShadowVisible: false,
-        headerTintColor: '#fff',
-        tabBarStyle: {
-          backgroundColor: '#25292e',
-        },
-      }}
-    >
-      <Tabs.Screen name="index" options={{
-        title: 'Home',
-      }} />
-      <Tabs.Screen name="about" options={{
-        title: 'About',
-      }} />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: "#ffd33d",
+                headerStyle: {
+                    backgroundColor: "#25292e",
+                },
+                headerShadowVisible: false,
+                headerTintColor: "#fff",
+                tabBarStyle: {
+                    backgroundColor: "#25292e",
+                },
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                }}
+            />
+            <Tabs.Screen
+                name="about"
+                options={{
+                    title: "About",
+                }}
+            />
+        </Tabs>
+    );
 }
